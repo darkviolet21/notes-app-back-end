0ce460b8f5d79f35e0f8184ab24ab726748b9075
darkviolet21 <m.irfan0205@gmail.com>
2023-03-23 12:40:31 +0700

    add fitur lihat , edit dan hapus

--- src/server.js
+++ src/server.js
@@ -5,6 +5,11 @@ const init = async () => {
   const server = Hapi.server({
     port: 5000,
     host: 'localhost',
+     routes: {
+    cors: {
+      origin: ['*'],
+    },
+  },
   });
  
    server.route(routes);
@@ -12,6 +17,7 @@ const init = async () => {
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
 };
+
  
  
 init();
